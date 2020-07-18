67e955ff18afffd8f7e0a09807b0b8c74e840e70
Jon Smart <jsmartware@gmail.com>
2020-07-18 13:38:27 -0700

    Using IEnumerable<T>

---
+++ Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace LINQExamples
+{
+    public class Employee 
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Threading;
 
 namespace LINQExamples
 {
@@ -6,7 +7,26 @@ namespace LINQExamples
     {
         static void Main(string[] args)
         {
-            
+            // IEnumerable hides the source of data, whether it's an Array...
+            IEnumerable<Employee> devs = new Employee[]
+            {
+                new Employee { EmpId = 1, EmpName = "Jon"},
+                new Employee { EmpId = 2, EmpName = "Harry" }
+            };
+
+            //... or a List<T>, etc.  
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { EmpId = 3, EmpName = "Marissa"}
+            };
+
+            // The IEnumerable<T> has a GetEnumerator() that allows us to walk through
+            // each item in the collection
+            foreach (var person in devs)
+            {
+                System.Console.WriteLine(person.EmpName);
+            }
+
         }
     }
 }
