cd7d631497c9dd40da4b1ab484b1b7dbd977cc4f
Jon Smart <jsmartware@gmail.com>
2020-07-20 15:30:15 -0700

    Added Queries project

---
+++ Queries/Movie.cs
@@ -0,0 +1,22 @@
+﻿namespace Queries
+{
+    public class Movie
+    {
+        private int _year;
+
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int Year
+        {
+            get
+            {
+                System.Console.WriteLine($"Returning {_year} for {Title}");
+                return _year;
+            }
+            set
+            {
+                _year = value;
+            }
+        }
+    }
+}
---
+++ Queries/MyLINQ.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Queries
+{
+    public static class MyLINQ
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            var result = new List<T>();
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        // Deferred execution Filter will act more like LINQ's Where() operator.
+        public static IEnumerable<T> DeferredExecutionFilter<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    // return one item at a time, only when requested by the caller.
+                    yield return item;
+                }
+            }
+        }
+    }
+}
